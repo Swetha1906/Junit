bec6c387afe2218935a31d72b1ef77774eb9e071
Swetha1906 <swethamurthy1917@gmail.com>
2019-12-16 15:26:48 +0530

    Junit

--- src/test/java/org/test/A.java
+++ src/test/java/org/test/A.java
@@ -1,5 +1,17 @@
 package org.test;
 
-public class A {
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class A {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\workspace\\Debugging\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+}
 }
