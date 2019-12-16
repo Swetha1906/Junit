23575e17a3c9fcb26dedabfef1660d19824b4800
Swetha1906 <swethamurthy1917@gmail.com>
2019-12-16 15:38:44 +0530

    Task Junit

--- src/test/java/org/test/A.java
+++ src/test/java/org/test/A.java
@@ -1,5 +1,18 @@
 package org.test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class A {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\workspace\\Debugging\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.greenstechnologys.com");
+		String text = driver.findElement(By.xpath("(//div[@class='box'])[2]")).getText();
+		System.out.println(text);
+		
+	}
+
 
 }
